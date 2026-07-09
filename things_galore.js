e11757ec1e0ba5c32651141e424669d1f42d72d9
Rydman Studios <ryder.fass@gmail.com>
2026-07-08 20:44:30 -0500

    Refactor reactive_powder reactions and properties

--- things_galore.js
+++ things_galore.js
@@ -1,14 +1,14 @@
 elements.reactive_powder = {
-  color: ["#ff00f7","#006eff","#00a383"],
-  behavior: behaviors.POWDER,
-  reactions: {
-    "sand": { elem1:"h_bomb", elem2:"bee", chance:0.5, oneway:true },
+	color: ["#ff00f7","#006eff","#00a383"],
+	behavior: behaviors.POWDER,
+	reactions: {
+		"sand": { elem1:"h_bomb", elem2:"bee", chance:0.5, oneway:true },
 		"gold": { elem1:"sand", chance:0.1, tempMax:-273, oneway:true},
 		"mulch": { elem1:"fire", chance:0.9, oneway:true},
-		"rock": { elem1:"reactive_powder", elem2:"rock", chance:1}
-  },
-  tempHigh: 1000000,
-  stateHigh: "magma",
+		"rock": { elem1:"reactive_powder", elem2:"rock", chance:1},
+	},
+	tempHigh: 1000000,
+	stateHigh: "magma",
 	category: "powders",
 	state: "solid",
 	density: 2550,
